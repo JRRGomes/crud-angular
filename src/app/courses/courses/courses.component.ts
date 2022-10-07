53316d92233d4bed095ce825ee8ee97c5cddf7ca
JRRGomes <jeffersonrolim@gmail.com>
2022-10-06 23:01:24 -0300

    feat: created course list array to feed the course table before using the service.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses: Course[] = [{ _id: '1', name: 'Java Web', category: 'Back-end' }];
+  displayedColumns = ['name', 'category'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
