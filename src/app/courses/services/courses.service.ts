6b8d5a0cfbf0f3a23e000995ef25fc23411e716f
JRRGomes <jeffersonrolim@gmail.com>
2022-10-09 21:45:18 -0300

    feat: created courses service that make requests for api. For now usinga a json as api endpoint.

---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Course } from '../model/course';
+import { first } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json'
+
+  constructor(private httpClient: HttpClient) { }
+
+  coursesList() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+    );
+  }
+}
